a51c67924db87985969c36d0d0671552a9701a0e
Valedelaf <valentina.delaf@gmail.com>
2024-06-08 17:39:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const precioBase = 400000;
+let cantidad = 0;
+let totalPagar= 0;
+
+document.querySelector('#precio').innerHTML = precioBase;
+document.querySelector('#cantidad').innerHTML = cantidad;
+document.querySelector('#totalPagar').innerHTML = totalPagar;
